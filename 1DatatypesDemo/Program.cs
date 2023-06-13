596b4c67b5bcf0c02cd413853bd715948444346e
vikul rathod <vikul.rathod.net@gmail.com>
2023-06-13 12:16:21 +0530

    Add project files.

---
+++ 1DatatypesDemo/Program.cs
@@ -0,0 +1,73 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Data Types Demo");
+
+        byte v1 = 10;
+        // v1 = 2500;
+        Console.WriteLine(v1); // 10
+        Console.WriteLine(sizeof(byte));
+        Console.WriteLine(byte.MinValue);
+        Console.WriteLine(byte.MaxValue);
+
+        short v2 = 10;
+        // v2 = 45000;
+        Console.WriteLine(v2); // 10
+        Console.WriteLine(sizeof(short));
+        Console.WriteLine(short.MinValue);
+        Console.WriteLine(short.MaxValue);
+
+        int v3 = 10;
+        Console.WriteLine(v3); // 10
+        Console.WriteLine(sizeof(int));
+        Console.WriteLine(int.MinValue);
+        Console.WriteLine(int.MaxValue);
+
+        long v4 = 10;
+        Console.WriteLine(v4); // 10
+
+        v4 = 20;
+        Console.WriteLine(v4); // 20
+        Console.WriteLine(sizeof(long));
+        Console.WriteLine(long.MinValue);
+        Console.WriteLine(long.MaxValue);
+
+        Console.WriteLine("------------------------------------------------------------");
+
+        float f1 = 45.456789f; // float value suffix with f
+        Console.WriteLine(f1);
+        Console.WriteLine(sizeof(float));
+        Console.WriteLine(float.MinValue);
+        Console.WriteLine(float.MaxValue);
+
+        double  f2= 45.456789;
+        Console.WriteLine(f2);
+        Console.WriteLine(sizeof(double));
+        Console.WriteLine(double.MinValue);
+        Console.WriteLine(double.MaxValue);
+
+        decimal f3 = 45.456789m; // decimal value suffix with m
+        Console.WriteLine(f3);
+        Console.WriteLine(sizeof(decimal));
+        Console.WriteLine(decimal.MinValue);
+        Console.WriteLine(decimal.MaxValue);
+
+        Console.WriteLine("------------------------------------------------------------");
+
+        bool b1 = true;
+        Console.WriteLine(b1);
+        Console.WriteLine(sizeof(bool));
+
+        Console.WriteLine("------------------------------------------------------------");
+
+        char c1 = 'A';
+        Console.WriteLine(c1);
+        Console.WriteLine(sizeof(char));
+
+        string name = "Vishal";
+        Console.WriteLine(name);       
+
+        Console.ReadLine();
+    }
+}
---
+++ Intro/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //Console.WriteLine("HELLO"); // to write on console screen
+        //Console.ReadLine(); // to read from console screen
+
+        Console.WriteLine("Welcome to Calculator APP !!!");
+
+        string choice = "";
+        do
+        {
+            Console.WriteLine("Please enter first number");
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter second number");
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter operation symbol");
+            char op = char.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+                case '+':
+                    Console.WriteLine($"Result: {n1} + {n2} = {n1 + n2}");
+                    break;
+                case '-':
+                    Console.WriteLine($"Result: {n1} - {n2} = {n1 - n2}");
+                    break;
+                case '*':
+                    Console.WriteLine($"Result: {n1} * {n2} = {n1 * n2}");
+                    break;
+                case '/':
+                    Console.WriteLine($"Result: {n1} / {n2} = {n1 / n2}");
+                    break;
+                default:
+                    Console.WriteLine($"Operation does not supported");
+                    break;
+            }
+            Console.WriteLine("Do you want to continue?");
+            choice = Console.ReadLine();
+        } while (choice == "yes" || choice == "y");
+
+        Console.WriteLine("Thank you ! Visit Again !!!");
+
+        Console.ReadLine(); // hold output screen
+    }
+}
