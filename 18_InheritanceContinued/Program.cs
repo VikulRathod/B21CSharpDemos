224bc07adb5995ba5115c355e06c9e44f56da4da
vikul rathod <vikul.rathod.net@gmail.com>
2023-06-30 12:31:39 +0530

    inheritance, base keyword, method hiding

---
+++ 18_InheritanceContinued/Program.cs
@@ -0,0 +1,69 @@
+﻿using _18_InheritanceContinued;
+
+class Progam
+{
+    static void Main()
+    {
+        //Customer c1 = new Customer();
+        //c1.AllShows();
+        //Console.WriteLine("Please provide your seat choice");
+        //string type = Console.ReadLine();
+
+        //int amount = c1.GetTicketAmount(type);
+        //Console.WriteLine($"Ticket Amount is {amount}");
+        //c1.PrintTicket();
+
+        Customer sc = new SilverCustomer();
+        // sc is a reference variable of type SilverCustomer class 
+        // pointing to SilverCustoer type object
+        sc.AllShows();
+        int amt = sc.GetTicketAmount();
+        Console.WriteLine(amt);
+        // sc.PrintTicket();
+        ((SilverCustomer)sc).PrintTicket();
+
+        Console.WriteLine("********************************************");
+
+        GoldCustomer gc = new GoldCustomer();
+        gc.AllShows();
+        amt = gc.GetTicketAmount();
+        Console.WriteLine(amt);
+        gc.PrintTicket();
+
+        Console.WriteLine("********************************************");
+
+        PlatinumCustomer pc = new PlatinumCustomer();
+        pc.AllShows();
+        amt = pc.GetTicketAmount();
+        Console.WriteLine(amt);
+        pc.PrintTicket();
+
+        Console.WriteLine("********************************************");
+
+        //Customer c1 = new Customer(); // bc => bc
+        //c1.AllShows();
+
+        //Customer c2 = new SilverCustomer(); // bc => dc
+        //Customer c3 = new GoldCustomer();
+        //Customer c4 = new PlatinumCustomer();
+
+        //SilverCustomer sc1 = new SilverCustomer(); // dc => dc
+        //SilverCustomer sc2 = (SilverCustomer) new Customer(); // not possible
+
+        //         int[] numbers = new int[3];
+        Customer[] customers = new Customer[3];
+        customers[0] = new SilverCustomer();
+        customers[1] = new GoldCustomer();
+        customers[2] = new PlatinumCustomer();
+
+        for (int i = 0; i < customers.Length; i++)
+        {
+            int amount = customers[i].GetTicketAmount();
+            Console.WriteLine(amount);
+            customers[i].PrintTicket();
+        }
+
+
+        Console.ReadLine();
+    }
+}
---
+++ 19_MethodHiding/Program.cs
@@ -0,0 +1,67 @@
+﻿class Program
+{
+    static void Main()
+    {
+        //A a1 = new A();
+        //a1.Print(); // A
+
+        //B b1 = new B();
+        //b1.Print(); // B
+
+        //A a2 = new B();
+        //a2.Print(); // A
+
+        //A a1 = new A();
+        //// A a2 = new B();
+        //B b1 = new B();
+
+        A a1 = new A("XYZ");
+        B b1 = new B("XYZ");
+
+        Console.ReadLine();
+    }
+}
+
+class A
+{
+    public string Name = "V#";
+
+    //public A()
+    //{
+    //    Console.WriteLine("A()");
+    //}
+
+    //public void Print()
+    //{
+    //    Console.WriteLine("A");
+    //}
+
+    public A(string s)
+    {
+        Console.WriteLine("A(string s)");
+        Console.WriteLine(Name);
+    }
+}
+
+class B : A
+{
+    public string Name = "V# TECH";
+
+    //public B()
+    //{
+    //    Console.WriteLine("B()");
+    //}
+
+    public B(string s) : base(s)
+    {
+        Console.WriteLine("B(string s)");
+        Console.WriteLine(Name);
+        Console.WriteLine(base.Name); // it will access base class Name value
+    }
+
+    // method hiding
+    //new public void Print()
+    //{
+    //    Console.WriteLine("B");
+    //}
+}
