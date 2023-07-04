72e19f1b5c57e69ab54f4cb7068255ca6a2ba64b
vikul rathod <vikul.rathod.net@gmail.com>
2023-07-04 13:33:38 +0530

    QA Session Examples

---
+++ 22_QASession/Program.cs
@@ -0,0 +1,128 @@
+﻿Student s1 = new Student() { RollNumber = 1, Name = "Kunal", City = "Pune" };
+s1.Print();
+
+#region direct copy
+//// 1 way to copy object
+//Student s2 = s1;
+//// s2.Print();
+
+//// problem
+//// City is different for s2 object
+//s2.City = "Mumbai";
+
+//s2.Print();
+
+//s1.Print(); // we are not expecting City as Mumbai for s1, City should be same Pune
+
+#endregion direct copy
+
+#region using parameterized constructor
+
+//Student s2 = new Student(s1.RollNumber, s1.Name, s1.City);
+//s2.Print();
+
+#endregion using parameterized constructor
+
+#region 3 using copy constructor
+
+Student s2 = new Student(s1);
+s2.City = "Mumbai";
+s2.Print();
+
+//Student s2 = new Student();
+//s2.RollNumber = s1.RollNumber;
+//s2.Name = s1.Name;
+//s2.City = s1.City();
+//s2.Print();
+
+#endregion 3 using copy constructor
+
+NumberSeries ns = new NumberSeries();
+Console.WriteLine("Please enter target number to print fibanacii series");
+int input = int.Parse(Console.ReadLine());
+ns.PrintFibonacciSeries(input);
+
+
+Console.WriteLine();
+B b = new B();
+
+Console.ReadLine();
+
+
+
+public class Student
+{
+    public int RollNumber;
+    public string Name;
+    public string City;
+    // many more
+
+    public Student() { }
+
+    public Student(int rn, string name, string city)
+    {
+        RollNumber = rn; Name = name; City = city;
+    }
+
+    public Student(Student s)
+    {
+        RollNumber = s.RollNumber; Name = s.Name; City = s.City;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine($"Details: RollNumber : {RollNumber} Name : {Name} City : {City}");
+    }
+
+    // return null value based on condition/ logic
+    public string GetValue(string name)
+    {
+        if (string.IsNullOrEmpty(name))
+        {
+            return null;
+        }
+        else
+        {
+            return $"Hello, {name}";
+        }
+    }
+}
+
+public class NumberSeries
+{
+    public void PrintFibonacciSeries(int lenght)
+    {
+        int n1 = 1;
+        int n2 = 1;
+
+        Console.Write($"{n1} {n2} ");
+        int n3 = 0;
+        for (int i = 1; (n1 + n2) < lenght; i++)
+        {
+            n3 = n1 + n2;
+            Console.Write($"{n3} ");
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
+
+
+class A
+{
+    public string Name = "A";
+    public A(string msg)
+    {
+        Console.WriteLine($"A(string msg) : {msg}");
+    }
+}
+class B : A
+{
+    public string Name = "B";
+    public B() : base("Hello")
+    {
+        Console.WriteLine("B()");
+        Console.WriteLine(Name); // B
+        Console.WriteLine(base.Name); // A
+    }
+}
---
+++ 22_QASession_Continued/Program.cs
@@ -0,0 +1,119 @@
+﻿#region ternary operator
+
+// int i = 10;
+
+#region if else with block
+
+//if (i == 10)
+//{
+//    Console.WriteLine("TEN");
+//}
+//else
+//{
+//    Console.WriteLine("NOT TEN");
+//}
+
+#endregion if else with block
+
+#region if else without block
+
+//// it is not mandatory to write curly braces for single line
+//// use block/ curly braces when we want to write multi line code
+//if (i == 10)
+//    Console.WriteLine("TEN");
+//else
+//    Console.WriteLine("NOT TEN");
+
+#endregion if else without block
+
+#region ternary operator
+
+//// (i == 10) ? Console.WriteLine("TEN") : Console.WriteLine("NOT TEN");
+
+//string result = (i == 10) ? "TEN" : "NOT TEN";
+//Console.WriteLine(result);
+
+//bool b1 = (i == 10) ? true : false;
+
+#endregion ternary operator
+
+#endregion ternary operator
+
+#region Parse() TryParse() methods
+
+//string input = "ABC";
+//// int i = input; // string  => int
+
+////int i1 = int.Parse(input);
+////Console.WriteLine(i1);
+
+//int i1;
+//bool status = int.TryParse(input, out i1);
+//Console.WriteLine($"Coversion Status : {status} Converted Value : {i1}");
+
+#endregion Parse() TryParse() methods
+
+#region bubble sort
+
+int[] numbers = new int[] { 2, 45, 14, 23, 3 }; // 5 / 0 - 4
+
+Console.WriteLine("Before Sort");
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{numbers[i]} ");
+}
+
+Console.WriteLine("\nAfter Sort");
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    // for (int j = (i + 1);i < numbers.Length - 1 && j < numbers.Length; j++)
+    for (int j = i; j < numbers.Length; j++)
+    {
+        //if (numbers[i] == numbers[j])
+        //{
+        //    continue;
+        //}
+        // descending order
+        //else if (numbers[i] < numbers[j])
+        //{
+        //    int temp = numbers[j];
+        //    numbers[j] = numbers[i];
+        //    numbers[i] = temp;
+        //}
+        // ascending order //  2, 45, 14, 23, 3
+        // 2,14, 45, 23, 3 // 2,14, 23, 45, 3 // 2,14, 23, 3, 45
+        if (numbers[i] > numbers[j])
+        {
+            int temp = numbers[j];
+            numbers[j] = numbers[i];
+            numbers[i] = temp;
+        }
+    }
+}
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.Write($"{numbers[i]} ");
+}
+
+#endregion bubble sort
+
+Console.ReadLine();
+
+namespace Name1
+{
+    public class A
+    {
+
+    }
+}
+
+namespace Name2
+{
+    using Name1;
+    public class B : A
+    {
+
+    }
+}
