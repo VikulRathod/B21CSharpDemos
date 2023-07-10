53ce8d63a94ac62e3b342308af7d789d36456124
vikul rathod <vikul.rathod.net@gmail.com>
2023-07-10 12:55:55 +0530

    operator overloading, abstract keyword

---
+++ 30_abstractKeyword/Customer.cs
@@ -0,0 +1,33 @@
+﻿public abstract class Customer
+{
+    private string _siteName;
+    public int TicketAmount { get; set; }
+
+    public Customer()
+    {
+        _siteName = "Flipkart";
+    }
+
+    public void Shows()
+    {
+        Console.WriteLine("*** ALL TODAYS SHOWS ***");
+    }
+
+    public abstract void Ticket();
+}
+
+public class SilverCustomer : Customer
+{
+    public override void Ticket()
+    {
+        Console.WriteLine("Silver Ticket Printed");
+    }
+}
+
+public class GoldCustomer : Customer
+{
+    public override void Ticket()
+    {
+        Console.WriteLine("Gold Ticket Printed");
+    }
+}
---
+++ 30_abstractKeyword/Program.cs
@@ -0,0 +1,19 @@
+﻿Customer c1 = new SilverCustomer();
+c1.Shows();
+
+Customer c2 = new GoldCustomer();
+c2.Shows();
+
+// Customer c = new Customer(); // error
+
+Customer c = new SilverCustomer();
+c.Shows(); // Customer method
+c.Ticket(); // SilverCustomer method
+
+Customer cc = new GoldCustomer();
+cc.Shows(); // Customer method
+cc.Ticket(); // SilverCustomer method
+
+
+Console.ReadLine();
+
