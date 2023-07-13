3938e48bba83f12269a02a8961705ec465ef54f9
vikul rathod <vikul.rathod.net@gmail.com>
2023-07-13 13:17:00 +0530

    partial class, generics demo

---
+++ 35_PartialKeyword/Kunal.cs
@@ -0,0 +1,21 @@
+﻿// Kunal
+
+public partial class Student : Course
+{
+    // public Student(){}
+    private string _city;
+    public int RollNumber { get; set; }
+    public string Name { get; set; }
+
+    // protected void Duplicate() { }
+
+    
+}
+
+interface IStudent
+{
+    void Print();
+}
+
+public class Course { }
+public class Institute { }
---
+++ 36_Generics/Program.cs
@@ -0,0 +1,67 @@
+﻿#region generic method
+
+//Calculator c1 = new Calculator();
+//bool b1 = c1.AreEqual(10, 10);
+//Console.WriteLine($"AreEqual(10, 10) : {b1}");
+
+//b1 = c1.AreEqual("Vishal", "Vishal");
+//Console.WriteLine($"AreEqual(\"Vishal\", \"Vishal\") : {b1}");
+
+//b1 = c1.AreEqual(10.5f, 21.5f);
+//Console.WriteLine($"AreEqual(10.5f, 21.5f) : {b1}");
+
+//b1 = c1.AreEqual<char>('$', '$');
+//Console.WriteLine($"AreEqual<char>('$', '$') : {b1}");
+
+// b1 = c1.AreEqual(10, "Vishal");
+
+#endregion generic method
+
+using System.Security.Cryptography;
+
+Calculator<int> c1 = new Calculator<int>();
+bool b1 = c1.AreEqual(10, 10);
+
+Calculator<string> c2 = new Calculator<string>();
+c2.AreEqual("A", "B");
+
+
+Console.ReadLine();
+
+//class Calculator
+//{
+//public bool AreEqual(int a, int b)
+//{
+//    return a == b;
+//}
+
+//public bool AreEqual(string a, string b)
+//{
+//    return a == b;
+//}
+
+//public bool AreEqual(float a, float b)
+//{
+//    return a == b;
+//}
+
+//    public bool AreEqual<T>(T a, T b)
+//    {
+//        return a.Equals(b); // same working like == operator
+//    }
+//}
+
+//class Calculator<T, T1, T2>
+class Calculator<T>
+{
+    //public T1 Property1 { get; set; }
+    //public T2 Property2 { get; set; }
+
+    public bool AreEqual(T a, T b)
+    {
+        return a.Equals(b);
+    }
+}
+
+// Calculator<string, string, float> c1 =
+// new Calculator<string, string, float>();
